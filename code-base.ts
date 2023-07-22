07a7d8c065c4dcb91e95ceccf3b752a8b41f13c5
Guilherme Severo <guilhermehmsevero@hotmail.com>
2023-07-22 14:23:22 -0300

    Added BKPs ignore and code-base

---
+++ code-base.ts
@@ -0,0 +1,29 @@
+// Create SingleSPA - Root or Applications
+npx create-single-spa
+
+// import React
+"react": "https://cdn.jsdelivr.net/npm/react@17.0.2/umd/react.production.min.js",
+"react-dom": "https://cdn.jsdelivr.net/npm/react-dom@17.0.2/umd/react-dom.production.min.js"
+
+// Add Single SPA into a created Vue application
+vue add single-spa
+
+// set-public-path.js
+import { setPublicPath } from 'systemjs-webpack-interop';
+
+setPublicPath('appName');
+
+// -----------------
+
+//Config do WebPack
+  chainWebpack(config) {
+    config.plugin('SystemJSPublicPathWebpackPlugin').tap((args) => {
+      args[0].rootDirectoryLevel = 1;
+      return args;
+    });
+  },
+  configureWebpack: {
+    output: {
+      libraryTarget: 'system',
+    },
+  },
